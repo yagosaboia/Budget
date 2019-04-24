4096e3c470678372b4b76d71a27bc24eb0daef9f
yagosaboia <yagosaboia@gmail.com>
2019-04-24 00:14:53 -0300

    Created new Storyboard for signIn and add some images on assets

---
+++ Budget/Controllers/LogInController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInController.swift
+//  Budget
+//
+//  Created by Yuri Saboia Felix Frota on 23/04/19.
+//  Copyright © 2019 com.yagoapps. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LogInController: UIViewController{
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        
+        //Get Default Auth UI Object
+        
+        //Set ourselves as delegate
+        
+        //Get a reference to the auth ui view controller
+        
+    }
+}
+
+
+
